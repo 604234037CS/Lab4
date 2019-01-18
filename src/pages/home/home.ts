fb3c89109ec0a14321470654672e4d6cc7b47caa
604234037CS <604234037@parichat.skru.ac.th>
2019-01-18 15:49:06 +0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+    
   constructor(public navCtrl: NavController) {
 
   }
+   
+  showBookList(categortName:string)
+  {
+    console.log(categortName);  
+  }
+
 
 }
